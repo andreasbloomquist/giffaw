2d914360ff2844e790c53e49f822408730b4e962
andreasbloomquist <andreasbloomquist@woboinc.com>
2015-05-12 00:31:26 -0700

    Minor refactors

--- base.js
+++ base.js
@@ -2,23 +2,26 @@ var getTrends = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&off
 var offset = 0;
 var clear = false;
 
+
 $(document).ready(function(){
 	// Function to show data
 	function render(dataSet){
     	dataSet.data.forEach(function(item,i){
     	var gif = dataSet.data[i].images.fixed_height.url;
-    	$("#pre").append("<img src='" + gif + "'>"); // mind the single vs. double quotes!
+    	$("#main").append("<img class='img-thumbnail'src='" + gif + "'>"); // mind the single vs. double quotes!
     	});
 	};
 
 	//Loads trending gifs on initial load
 	$.get(getTrends, render);
 
+	// Event listener on input field for new search strings.
+	// Text entered in th e
 	$('input').on("change", function (event) {
      	var	searchApi = 'http://api.giphy.com/v1/gifs/search?q=' + event.target.value + '&api_key=dc6zaTOxFJmzC';
      	// Removes current images and replaces using search input
      	$('img').remove();
-     	$.get(searchApi, render);
+		$.get(searchApi, render);
 	});
 
 	//Loads 25 more gifs upon click, although rendering this many gifs on the page takes a long time to load.
